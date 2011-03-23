1488a455060e0b69ef19bc4f3ca19c6dbf4e9850
luyuanzheng <luyuanzheng1209@gmail.com>
2011-03-23 21:37:35 +0800

    resolve the problem of the git

--- src/com/transformer/test/compiler/TestJobStruct.java
+++ src/com/transformer/test/compiler/TestJobStruct.java
@@ -3,5 +3,6 @@ package com.transformer.test.compiler;
 public class TestJobStruct {
 	public static void main(String[] args) {
 		System.out.println("test JobStruct");
+		System.out.println("test JobStruct");
 	}
 }
